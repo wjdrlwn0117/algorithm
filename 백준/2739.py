6abe62c03aa4ee4bc86e2de0c3fa2a7467d66df8
wjdrlwn0117 <77970721+wjdrlwn0117@users.noreply.github.com>
2022-02-21 02:45:34 +0900

    Revert "2438, 2439 2739"

--- 백준/2739.py
+++
@@ -1,9 +0,0 @@
-num = range(1, 10)
-num = int(input())
-
-for i in range(1, 10):
-    print(str(num) + " * " + str(i) + " = " + str((num * i)))
-
-
-# 파이썬에서 문자와 숫자를 함께 출력하는 방식을 배움
-# 숫자를 문자로 str(), 문자를 숫자(정수)로 int()
